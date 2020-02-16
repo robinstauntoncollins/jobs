4b2f56d568562d7434eb4eb60cab9c39d2aab34e
Robin Staunton-Collins <robin.staunton-collins@capgemini.com>
2020-02-16 22:02:16 +0100

    Modified to use flask-restful

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 from flask import Flask, jsonify, abort, make_response, request, url_for
 from flask_httpauth import HTTPBasicAuth
-app = Flask(__name__)
+from flask_restful import Api, Resource, reqparse, fields, marshal
+
+app = Flask(__name__, static_url_path="")
+api = Api(app)
 
 auth = HTTPBasicAuth()
+
+@auth.get_password
+def get_password(username):
+    if username == 'robin':
+        return 'python'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
+
+
 
 jobs = [
     {
@@ -25,107 +40,86 @@ jobs = [
     }
 ]
 
-@app.route('/jobs/api/v1.0/jobs', methods=['GET'])
-@auth.login_required
-def get_jobs():
-    return jsonify({'jobs': [make_public_job(job) for job in jobs]})
+job_fields = {
+    'title': fields.String,
+    'location': fields.String,
+    'frequency': fields.String,
+    'preferred': fields.String,
+    'description': fields.String,
+    'done': fields.Boolean,
+    'uri': fields.Url('job')
+}
 
+class JobListAPI(Resource):
 
-@app.route('/jobs/api/v1.0/jobs/<int:job_id>', methods=['GET'])
-@auth.login_required
-def get_job(job_id):
-    job = [job for job in jobs if job['id'] == job_id]
-    if len(job) == 0:
-        abort(404)
-    return jsonify({'job': make_public_job(job[0])})
+    decorators = [auth.login_required]
 
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type=str, required=True, help="No job title provided", location="json")
+        self.reqparse.add_argument('description', type=str, default="", location="json")
+        self.reqparse.add_argument('location', type=str, required=True, location="json", help="No location provided")
+        self.reqparse.add_argument('frequency', type=str, default="weekly", location="json")
+        self.reqparse.add_argument('preferred', type=str, default="", location="json")
+        super(JobListAPI, self).__init__()
 
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    def get(self):
+        return {'jobs': [marshal(job, job_fields) for job in jobs]}
 
+    def post(self):
+        args = self.reqparse.parse_args()
+        job = {
+            'id': jobs[-1]['id'] + 1 if len(jobs) > 0 else 1,
+            'title': args['title'],
+            'description': args['description'],
+            'location': args['location'],
+            'frequency': args['frequency'],
+            'preferred': args['preferred'],
+            'done': False
+        }
+        jobs.append(job)
+        return {'job': marshal(job, job_fields)}, 201
 
-@app.errorhandler(400)
-def bad_request(error):
-    return make_response(jsonify({'error': "Bad Request"}), 400)
+class JobAPI(Resource):
 
+    decorators = [auth.login_required]
 
-@app.route('/jobs/api/v1.0/jobs', methods=['POSt'])
-@auth.login_required
-def create_job():
-    if not request.json or not 'title' in request.json or not 'location' in request.json or not 'frequency' in request.json:
-        abort(400)
-    job = {
-        'id': jobs[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
-        'location': request.json['location'],
-        'frequency': request.json['frequency'],
-        'preferred': request.json.get('preferred', ""),
-        'done': False
-    }
-    jobs.append(job)
-    return jsonify({'job': make_public_job(job)}), 201
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type=str, location="json")
+        self.reqparse.add_argument('description', type=str, default="", location="json")
+        self.reqparse.add_argument('location', type=str, location="json")
+        self.reqparse.add_argument('frequency', type=str, default="weekly", location="json")
+        self.reqparse.add_argument('preferred', type=str, default="", location="json")
+        self.reqparse.add_argument('done', type=bool, default=False, location="json")
+        super(JobAPI, self).__init__()
 
+    def get(self, id):
+        job = [job for job in jobs if job['id'] == id]
+        if len(job) == 0:
+            abort(404)
+        return {'job': marshal(job[0], job_fields)}
 
-@app.route('/jobs/api/v1.0/jobs/<int:job_id>', methods=['PUT'])
-@auth.login_required
-def update_job(job_id):
-    job = [job for job in jobs if job['id'] == job_id]
-    if len(job) == 0:
-        abort(404)
-    if not request.json:
-        abort(400)
-    if 'title' in request.json and type(request.json['title']) != unicode:
-        abort(400)
-    if 'description' in request.json and type(request.json['description']) != unicode:
-        abort(400)
-    if 'location' in request.json and type(request.json['location']) != unicode:
-        abort(400)
-    if 'frequency' in request.json and type(request.json['frequency']) != unicode:
-        abort(400)
-    if 'preferred' in request.json and type(request.json['preferred']) != unicode:
-        abort(400)
-    if 'done' in request.json and type(request.json['done']) != bool:
-        abort(400)
-    job[0]['title'] = request.json.get('title', job[0]['title'])
-    job[0]['description'] = request.json.get('description', job[0]['description'])
-    job[0]['location'] = request.json.get('location', job[0]['location'])
-    job[0]['frequency'] = request.json.get('frequency', job[0]['frequency'])
-    job[0]['preferred'] = request.json.get('preferred', job[0]['preferred'])
-    job[0]['done'] = request.json.get('done', job[0]['done'])
-    return jsonify({'job': make_public_job(job[0])})
+    def put(self, id):
+        job = [job for job in jobs if job['id'] == id]
+        if len(job) == 0:
+            abort(404)
+        job = job[0]
+        args = self.reqparse.parse_args()
+        for k, v in args.items():
+            if v is not None:
+                job[k] = v
+        return {'job': marshal(job, job_fields)}
 
+    def delete(self, id):
+        job = [job for job in jobs if job['id'] == id]
+        if len(job) == 0:
+            abort(404)
+        jobs.remove(job[0])
+        return {'result': True}
 
-@app.route('/jobs/api/v1.0/jobs/<int:job_id>', methods=['DELETE'])
-@auth.login_required
-def delete_job(job_id):
-    job = [job for job in jobs if job['id'] == job_id]
-    if len(job) == 0:
-        abort(404)
-    jobs.remove(job[0])
-    return jsonify({'result': True})
-
-
-def make_public_job(job):
-    new_job = {}
-    for field in job:
-        if field == 'id':
-            new_job['uri'] = url_for('get_job', job_id=job['id'], _external=True)
-        else:
-            new_job[field] = job[field]
-    return new_job
-
-
-@auth.get_password
-def get_password(username):
-    if username == 'robin':
-        return 'python'
-    return None
-
-@auth.error_handler
-def unauthorized():
-    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
+api.add_resource(JobListAPI, '/jobs/api/v1.0/jobs', endpoint='jobs')
+api.add_resource(JobAPI, '/jobs/api/v1.0/jobs/<int:id>', endpoint='job')
 
 
 if __name__ == '__main__':
